f9032a3751a8861501cbb13eac1fa3fa32d09814
oussama <oussama.houssami1@gmail.com>
2019-12-16 16:45:34 +0200

    javascript basics02-s1-s2

---
+++ Exercises/Javascript-Basics-02/step 1/step_1.js
@@ -0,0 +1,3 @@
+document.querySelector("#image1").addEventListener("mouseover", function() {
+  this.src = "images/image1_2.jpg";
+});
---
+++ Exercises/Javascript-Basics-02/step 2/step_2.js
@@ -0,0 +1,3 @@
+document.querySelector("#name").addEventListener("focusout", function() {
+  alert("thank you for participating!");
+});
