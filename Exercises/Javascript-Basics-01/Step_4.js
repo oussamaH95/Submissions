512a53e4222e704e1fae9435d0239ff9e72c8699
oussama <oussama.houssami1@gmail.com>
2019-12-16 15:00:46 +0200

    javascript basics01-s4

---
+++ Exercises/Javascript-Basics-01/Step_4.js
@@ -0,0 +1,6 @@
+function f1() {
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+  alert("name = " + name + "\nsurname = " + surname + "\ncity = " + city);
+}
