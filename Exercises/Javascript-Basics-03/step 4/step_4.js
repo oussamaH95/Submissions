66bda921f08687fbd9eef325b93f6285af09b272
oussama <oussama.houssami1@gmail.com>
2019-12-17 16:06:51 +0200

    javascript basics03 s4

---
+++ Exercises/Javascript-Basics-03/step 4/step_4.js
@@ -0,0 +1,6 @@
+function f1(a, b) {
+  if (a != b) {
+    document.getElementById("password").style.borderColor = "red";
+    document.getElementById("confirmation").style.borderColor = "red";
+  }
+}
