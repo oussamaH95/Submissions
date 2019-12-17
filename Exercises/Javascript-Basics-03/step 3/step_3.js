0fd378458fd7e1c6041a6f2ec1a435a372addaf8
oussama <oussama.houssami1@gmail.com>
2019-12-17 15:53:04 +0200

    javascript basics03 s3

---
+++ Exercises/Javascript-Basics-03/step 3/step_3.js
@@ -0,0 +1,5 @@
+function f1(a) {
+  if (a == "green") document.getElementById("text").style.color = "green";
+  else if (a == "red") document.getElementById("text").style.color = "red";
+  else document.getElementById("text").style.color = "blue";
+}
