a4bf67e36754bfda70863b7343beda3303033dac
oussama <oussama.houssami1@gmail.com>
2019-12-18 14:22:17 +0200

    javascript basics02 edit s5 s6

--- Exercises/Javascript-Basics-02/step 5/step_5.js
+++ Exercises/Javascript-Basics-02/step 5/step_5.js
@@ -1,3 +1,7 @@
-function f1(a, b) {
-  document.querySelector(a).src = b;
+var x = document.querySelectorAll("img");
+
+for (let i = 0; i < 5; i++) {
+  x[i].addEventListener("mouseover", function() {
+    x[i].src = "images/image" + (i + 1) + "_2.jpg";
+  });
 }
--- Exercises/Javascript-Basics-02/step 6/step_6.js
+++ Exercises/Javascript-Basics-02/step 6/step_6.js
@@ -1,3 +1,12 @@
-function f1(a, b) {
-  document.querySelector(a).src = b;
+var imageLink;
+var x = document.querySelectorAll("img");
+for (let i = 0; i < 5; i++) {
+  x[i].addEventListener("mouseover", function() {
+    x[i].src = "images/image" + (i + 1) + "_2.jpg";
+  });
+  x[i].addEventListener("mouseout", function() {
+    imageLink = x[i].src;
+    imageLink = imageLink.replace("_2", "");
+    x[i].src = imageLink;
+  });
 }
