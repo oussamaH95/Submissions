78315da345b0bd0b9997fe9e1fff66d775486041
oussama <oussama.houssami1@gmail.com>
2019-12-18 14:48:02 +0200

    javascript basics03 s 2 edit

--- Exercises/Javascript-Basics-03/step 2/step_2.js
+++ Exercises/Javascript-Basics-03/step 2/step_2.js
@@ -1,4 +1,12 @@
-function f1(a) {
-  if (a == "hide") document.getElementById("texte").style.opacity = "0.0";
-  else document.getElementById("texte").style.opacity = "1.0";
+//function f1(a) {
+//  if (a == "hide") document.getElementById("texte").style.opacity = "0.0";
+// else document.getElementById("texte").style.opacity = "1.0";
+//}
+var x = document.querySelectorAll("a");
+for (let i = 0; i < 2; i++) {
+  x[i].addEventListener("click", function() {
+    if (x[i].id == "show")
+      document.getElementById("texte").style.opacity = "1.0";
+    else document.getElementById("texte").style.opacity = "0.0";
+  });
 }
