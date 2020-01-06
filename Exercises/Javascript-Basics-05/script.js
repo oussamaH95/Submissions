7ef28c50f96dd4575e1da3e317a57c92d30a2f2a
oussama <oussama.houssami1@gmail.com>
2020-01-06 19:58:42 +0200

    loop

--- Exercises/Javascript-Basics-05/script.js
+++ Exercises/Javascript-Basics-05/script.js
@@ -1,6 +1,5 @@
-let name = prompt("enter ur name");
-if (name != "Alice" && name != "Bob") {
-  name = "Stranger";
-}
+let n = prompt("enter a number");
+let sum = 0;
+for (let i = 1; i <= n; i++) sum = sum + i;
 
-window.alert("Hello " + name);
+window.alert("ur sum is " + sum);
