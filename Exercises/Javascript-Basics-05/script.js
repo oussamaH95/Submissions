2d4a9c1e500c0ab910c79f7a83dbedc8d9b1b14c
oussama <oussama.houssami1@gmail.com>
2020-01-06 20:01:25 +0200

    multiples

--- Exercises/Javascript-Basics-05/script.js
+++ Exercises/Javascript-Basics-05/script.js
@@ -1,5 +1,7 @@
 let n = prompt("enter a number");
 let sum = 0;
-for (let i = 1; i <= n; i++) sum = sum + i;
+for (let i = 1; i <= n; i++) {
+  if (i % 3 == 0 || i % 5 == 0) sum = sum + i;
+}
 
 window.alert("ur sum is " + sum);
