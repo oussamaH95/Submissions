01d1e7e56c810ac9aaf389a0475813aae095f23a
oussama <oussama.houssami1@gmail.com>
2020-01-08 12:59:44 +0200

    guessing game

--- Exercises/Javascript-Basics-05/script.js
+++ Exercises/Javascript-Basics-05/script.js
@@ -1,8 +1,15 @@
-let str = "";
-let isPrime = true;
-for (let i = 2; i <= 30; i++) {
-  isPrime = true;
-  for (let j = 2; j < i; j++) if (i % j == 0 && isPrime) isPrime = false;
-  if (isPrime) str = str + " " + i;
-}
-window.alert(str);
+var gessNum = Math.floor(Math.random() * 100) + 1;
+var count = 0;
+document.getElementById("guess").onclick = function() {
+  var num = document.getElementById("num").value;
+
+  if (num == gessNum) {
+    alert(" Correct with " + count + " trials! ");
+  } else if (num > gessNum) {
+    count++;
+    alert("Too Large trial " + count + "/8");
+  } else {
+    count++;
+    alert("Too Small trial " + count + "/8");
+  }
+};
