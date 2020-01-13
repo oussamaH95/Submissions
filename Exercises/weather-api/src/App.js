d87d4e15e4b9618150710684cd28c232f3adff2b
oussama <oussama.houssami1@gmail.com>
2020-01-13 16:31:35 +0200

    Weather API: HTML/CSS

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
 
 class App extends Component {
   constructor(props) {
@@ -11,9 +12,58 @@ class App extends Component {
     return (
       <div className="app">
         <header className="app__header">
-          <h1>Hello World!</h1>
+          <input type="text" placeholder="Enter a Location"></input>
+          <button>Find Weather</button>
         </header>
-        <main className="app__main"></main>
+        <main className="app__main">
+          <div class="mainWeather">
+            <img class="mainImg" src={mostlycloudy}></img>
+            <caption>overcast cloud</caption>
+            <p>
+              <strong>temperature </strong> 10°C to 11°C
+            </p>
+            <p>
+              <strong>Humedty </strong> 78% <strong>pressure </strong> 100848
+            </p>
+          </div>
+          <div class="allWeather">
+            <div class="weatherTime">
+              <time>03:00</time>
+              <img src={mostlycloudy}></img>
+              <p>8°C</p>
+            </div>
+            <div class="weatherTime">
+              <time>03:00</time>
+              <img src={mostlycloudy}></img>
+              <p>8°C</p>
+            </div>
+            <div class="weatherTime">
+              <time>03:00</time>
+              <img src={mostlycloudy}></img>
+              <p>8°C</p>
+            </div>
+            <div class="weatherTime">
+              <time>03:00</time>
+              <img src={mostlycloudy}></img>
+              <p>8°C</p>
+            </div>
+            <div class="weatherTime">
+              <time>03:00</time>
+              <img src={mostlycloudy}></img>
+              <p>8°C</p>
+            </div>
+            <div class="weatherTime">
+              <time>03:00</time>
+              <img src={mostlycloudy}></img>
+              <p>8°C</p>
+            </div>
+            <div class="weatherTime">
+              <time>03:00</time>
+              <img src={mostlycloudy}></img>
+              <p>8°C</p>
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
