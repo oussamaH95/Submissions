a863dccba3a0e35ee777eabfb5d52fbaa8c89bb0
oussama <oussama.houssami1@gmail.com>
2020-01-13 10:48:29 +0200

    add api folder

--- Exercises/weather-api/src/App.js
+++ Exercises/weather-api/src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <header className="app__header"></header>
+        <header className="app__header">
+          <h1>Hello World!</h1>
+        </header>
         <main className="app__main"></main>
       </div>
     );
