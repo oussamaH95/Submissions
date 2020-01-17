12c8d9336ee11f3b449109849d35119e03d7f7a2
oussama <oussama.houssami1@gmail.com>
2020-01-17 14:18:53 +0200

    Basic Queries

---
+++ Exercises/SQLite Basic/answers.sh
@@ -0,0 +1,8 @@
+select Name from students;
+select * from students where Age>30;
+select * from students where Age>30 and Gender='F';
+select Points from students where Name='Alex';
+insert into students values(7,'Oussama',24,'M',1000);
+update students set Points = 500 where name = 'Basma'
+update students set Points = 50 where name = 'Alex'
+
