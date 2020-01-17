4fcbd5c32e0ab1c6f572b6c6f3f3dd1b50610637
oussama <oussama.houssami1@gmail.com>
2020-01-17 14:27:12 +0200

    Execute SQL

--- Exercises/SQLite Basic/answers.sh
+++ Exercises/SQLite Basic/answers.sh
@@ -5,4 +5,11 @@ select Points from students where Name='Alex';
 insert into students values(7,'Oussama',24,'M',1000);
 update students set Points = 500 where name = 'Basma'
 update students set Points = 50 where name = 'Alex'
-
+create tablte graduate(
+   id int not null primary key auto_increment,
+   name varchar2(30) not null unique,
+   age int(2),
+   gender varchar2(6),
+   Points int(4),
+   graduation varchar2(15)
+)
