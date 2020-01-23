30c5a9b47fd178bc956ccd7680a9a0567763da81
oussama <oussama.houssami1@gmail.com>
2020-01-23 11:53:04 +0200

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -20,7 +20,58 @@ app.get("/time", (req, res) => {
     message: t
   });
 });
-
+app.get("/time", (req, res) => {
+  res.send({
+    status: 200,
+    message: t
+  });
+});
+app.get("/hello/:id", (req, res) => {
+  let id = req.params.id;
+  res.send({
+    status: 200,
+    message: "hello " + id
+  });
+});
+app.get("/search", (req, res) => {
+  if (req.query.s) {
+    res.send({
+      status: 200,
+      message: "ok",
+      data: req.query.s
+    });
+  } else {
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    });
+  }
+});
 app.listen(port, () => {
   console.log(`server is running at: http://localhost:${port}/`);
+});
+app.get("movies/crate", (req, res) => {
+  res.send({
+    status: 200,
+    message: "this is create page"
+  });
+});
+app.get("movies/read", (req, res) => {
+  res.send({
+    status: 200,
+    message: "this is read page"
+  });
+});
+app.get("movies/update", (req, res) => {
+  res.send({
+    status: 200,
+    message: "this is update page"
+  });
+});
+app.get("movies/delete", (req, res) => {
+  res.send({
+    status: 200,
+    message: "this is delete page"
+  });
 });
